6a3784a25bf888e259394b2c20339d4e71972bb8
muhammadzidane <muhammmadzidanealsaadawi@gmail.com>
2021-06-27 21:03:37 +0700

    Home page, Status page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,28 +1,39 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
 import iconPixbox from "../../assets/images/logo/logo-white-3.png";
 
-export default class Home extends Component {
+export default class HomePage extends Component {
   render() {
-    return (
-      <div class="b-body b-body-with-navbar">
-        <div class="b-container">
-          <div class="b-navbar">
-            <div class="b-navbar-container">
+      return (
+        <div class="b-body b-body-with-navbar">
+          <div class="b-container">
+            <div class="b-navbar">
+              <div class="b-navbar-container">
               <div class="b-navbar-wrapper">
-                <div class="b-navbar-logo">
+                  <div class="b-navbar-logo">
                   <img src={iconPixbox} />
-                </div>
-                <div class="b-navbar-menu">
+                  </div>
+                  <div class="b-navbar-menu">
                   <a href="#">Login</a>
-                </div>
+                  </div>
+              </div>
+              </div>
+            </div>
+            <div className='b-home-content'>
+              <div className="b-tbold-1 b-px-1">Masukan nomer resi untuk melihat status order Anda ?</div>
+              <div>
+                <form action="#">
+                  <div className="b-form-gp">
+                    <div className="b-p-relative">
+                      <input className="b-input-form b-w-100" name="no_resi" placeholder="Masukan nomer resi ..."/>
+                      <span className="b-search-icon">S</span>
+                    </div>
+                    <button className="b-w-100 b-mt-10 b-btn-order b-tbold b-c-white">Order Baru</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
-          <h1>WANT TO SEE EXAMPLE ?</h1>
-          <Link to="example">SEE EXAMPLE</Link>
         </div>
-      </div>
-    );
+      )
   }
 }
