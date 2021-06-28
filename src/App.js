39ecf9820a2ac8bb3d437764b0d15049f1b540d6
Encep Ihwan <ihwanencep@gmail.com>
2021-06-28 09:31:58 +0700

    page order, page struck

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import home from "./pages/home/index.js";
 import example from "./pages/example/index.js";
+import order from "./pages/order/index.js";
+import struck from "./pages/struck/index.js";
 import login from "./pages/login/index.js";
 import "./assets/scss/main.scss";
 
@@ -12,6 +14,8 @@ function App() {
         <Route exact path="/" component={home} />
         <Route exact path="/home" component={home} />
         <Route exact path="/example" component={example} />
+        <Route exact path="/order" component={order} />
+        <Route exact path="/struck" component={struck} />
         <Route exact path="/login" component={login} />
       </Router>
     </div>
