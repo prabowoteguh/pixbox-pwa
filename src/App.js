cff7dd766f5860ff4374b615e2e160f88214bb9b
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2021-06-29 20:40:08 +0700

    Fix losing code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//#region - IMPORT PAGES
 import home from "./pages/home/index.js";
 import example from "./pages/example/index.js";
 import login from "./pages/login/index.js";
 import statusPage from "./pages/status/index.js";
 import struck from "./pages/struck/index.js";
+import order from "./pages/order/index.js";
+//#endregion - IMPORT PAGES
 
+//#region - IMPORT STYLE
+import "./assets/feather-icon/feather-icon.css";
 import "./assets/scss/main.scss";
+//#endregion - IMPORT STYLE
 
 function App() {
   return (
@@ -17,6 +24,7 @@ function App() {
         <Route exact path="/example" component={example} />
         <Route exact path="/login" component={login} />
         <Route exact path="/status" component={statusPage} />
+        <Route exact path="/order" component={order} />
         <Route exact path="/struck" component={struck} />
       </Router>
     </div>
