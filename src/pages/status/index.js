eef8ed0be9b77e3c6ca7a4067f440b4d8b10bd5a
muhammadzidane <muhammmadzidanealsaadawi@gmail.com>
2021-07-10 16:19:16 +0700

    Update status page - menampilkan tombol status

--- src/pages/status/index.js
+++ src/pages/status/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { Component } from "react";
 import iconPixbox from "../../assets/images/logo/logo-white-3.png";
-import { useLocation, Link, useParams } from "react-router-dom";
 import Navbar from "../../parts/navbar.js";
 
 export default class StatusPage extends Component {
@@ -9,48 +8,77 @@ export default class StatusPage extends Component {
     super();
 
     this.state = {
-      months: [
-        "Januari",
-        "Februari",
-        "Maret",
-        "April",
-        "Mei",
-        "Juni",
-        "Juli",
-        "Agustus",
-        "September",
-        "Oktober",
-        "November",
-        "Desember",
-      ],
-      detailOrder: {},
+      datas: {},
+      orderStatus: "",
     };
   }
 
   componentDidMount() {
-    const resiCode = this.props.match.params.id;
-    let url = "api/orders/" + resiCode;
+    let searchValue = this.props.location.state.search.toUpperCase();
+    let url = `/api/orders/${searchValue}`;
+
     axios.get(url).then((response) => {
       this.setState({
-        detailOrder: response.data.data,
+        datas: response.data.data,
+        orderStatus: response.data.data.orderStatus.length,
       });
     });
   }
-
   render() {
-    const detailOrder = this.state.detailOrder;
-    let orderList = [];
-    if (typeof detailOrder.orderStatus != "undefined") {
-      orderList = detailOrder.orderStatus;
+    const data = this.state.datas;
+    const orderStatus = this.state.orderStatus;
+    const mount = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+
+    let dateOrderDate = new Date(data.order_date);
+    let orderDate = `${dateOrderDate.getDate()} ${mount[dateOrderDate.getMonth()]} `;
+    orderDate += dateOrderDate.getFullYear();
+
+    let process, pickup, deliver, complete, pickupCourier, deliverCourier;
+
+    if (orderStatus == 1) {
+      process = "b-status-circle";
+      pickup = "b-status-circle b-status-none";
+      deliver = "b-status-circle b-status-none";
+      complete = "b-status-circle b-status-none";
+      pickupCourier = "b-status-courier-name b-status-courier-none";
+      deliverCourier = "b-status-courier-name b-status-courier-none";
+    } else if (orderStatus == 2) {
+      process = "b-status-circle b-status-none";
+      pickup = "b-status-circle";
+      deliver = "b-status-circle b-status-none";
+      complete = "b-status-circle b-status-none";
+      pickupCourier = "b-status-courier-name";
+      deliverCourier = "b-status-courier-name b-status-courier-none";
+    } else if (orderStatus == 3) {
+      process = "b-status-circle b-status-none";
+      pickup = "b-status-circle b-status-none";
+      deliver = "b-status-circle";
+      complete = "b-status-circle b-status-none";
+      pickupCourier = "b-status-courier-name";
+      deliverCourier = "b-status-courier-name";
+    } else if (orderStatus == 4) {
+      process = "b-status-circle b-status-none";
+      pickup = "b-status-circle b-status-none";
+      deliver = "b-status-circle b-status-none";
+      complete = "b-status-circle";
+      pickupCourier = "b-status-courier-name";
+      deliverCourier = "b-status-courier-name";
     }
 
-    const { months } = this.state;
-
-    let dateOrderDate = new Date(detailOrder.order_date);
-    let orderDate = `${dateOrderDate.getDate()} ${
-      months[dateOrderDate.getMonth()]
-    } `;
-    orderDate += dateOrderDate.getFullYear();
+    console.log(orderStatus);
 
     return (
       <div className="b-body b-body-with-navbar">
@@ -60,70 +88,57 @@ export default class StatusPage extends Component {
             <div>
               <div>{orderDate}</div>
               <div className="b-tbold-2 b-f-16">
-                <div className="b-mt-4">{detailOrder.order_code}</div>
+                <div className="b-mt-4">{data.order_code}</div>
                 <div>Boltz Regular</div>
               </div>
               <div className="b-mt-6">
                 <div className="b-status-list">
-                  {/* {orderList.map((item, index) => (
-                    <div className="b-status">
-                      <div className="b-status-path">
-                        <div className="b-status-circle"></div>
-                      </div>
-                      <div className="b-status-info">
-                        <div className="b-status-title">{item.status}</div>
-                        <div className="b-status-description">
-                          Menunggu paket diproses oleh {item.users.fullname}
-                        </div>
-                      </div>
-                    </div>
-                  ))} */}
                   <div className="b-status">
                     <div className="b-status-path">
-                      <div className="b-status-circle b-status-none"></div>
+                      <div className={process}></div>
                     </div>
                     <div className="b-status-info">
                       <div className="b-status-title">Process</div>
                       <div className="b-status-description">
-                        Menunggu paket diprosess
+                        Menunggu paket diproses
                       </div>
                     </div>
                   </div>
                   <div className="b-status">
                     <div className="b-status-path">
-                      <div className="b-status-circle b-status-none"></div>
+                      <div className={pickup}></div>
                     </div>
                     <div className="b-status-info">
                       <div className="b-status-title">Pickup</div>
                       <div className="b-status-description">
                         Menunggu paket dipickup oleh kurir kami
                       </div>
-                      <div className="b-status-courier-name">
+                      <div className={pickupCourier}>
                         <span>Kurir: </span>
-                        <span>{detailOrder.pickup_name}</span>
-                        <span>({detailOrder.pickup_phone})</span>
+                        <span>{data.pickup_name}</span>
+                        <div>({data.pickup_phone})</div>
                       </div>
                     </div>
                   </div>
                   <div className="b-status">
                     <div className="b-status-path">
-                      <div className="b-status-circle"></div>
+                      <div className={deliver}></div>
                     </div>
                     <div className="b-status-info">
                       <div className="b-status-title">Deliver</div>
                       <div className="b-status-description">
                         Paket sedang dalam perjalanan menuju tujuan
                       </div>
-                      <div className="b-status-courier-name">
+                      <div className={deliverCourier}>
                         <span>Kurir: </span>
-                        <span>{detailOrder.deliver_name}</span>
-                        <span>({detailOrder.deliver_phone})</span>
+                        <span>{data.deliver_name}</span>
+                        <div>({data.deliver_phone})</div>
                       </div>
                     </div>
                   </div>
                   <div className="b-status">
                     <div className="b-status-path">
-                      <div className="b-status-circle b-status-none"></div>
+                      <div className={complete}></div>
                     </div>
                     <div className="b-status-info">
                       <div className="b-status-title">Complete</div>
